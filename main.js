55c9a50cc49bd623e5aab74f58bd090b60683966
Danylo Vovchok <vovchok.danya01@gmail.com>
2024-05-01 12:38:39 +0200

    include dashboard route in main js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const dashboardRoutes = require('./routes/dashboardRoutes');
 const app = express();
 const port = 3000;
 
@@ -9,6 +10,8 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(authRoutes);
+
+app.use(dashboardRoutes);
 
 
 app.get('/', (req, res) => {
